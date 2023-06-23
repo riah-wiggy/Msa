e42941fdec96db9f8f356034fb4ef6e5bb971913
riah-wiggy <136471946+riah-wiggy@users.noreply.github.com>
2023-06-23 11:24:43 -0500

    Add files via upload

--- read_files.py
+++ read_files.py
@@ -5,16 +5,10 @@ student = {}
 
 #loop through data in the file
 for line_of_data in data_file:
-#what do we need to do to each line of data
-#split line of data at the ","(string
-#create an entry to the dictionary
     key_values = line_of_data.split(", ")
-
-#create an enrty to the dictionary. remember to covert price to float
-student[key_values[0]] = float(key_values[1])
 
 #close file
 data_file.close()
 
-for item, price in student.items():
-    print(f"{item}: ${price:.2f}") 
+for student, info in student.items():
+    print()
--- student_generator.py
+++ student_generator.py
@@ -1,13 +1,68 @@
 import Student
+from datetime import datetime
 #create 2 students
 
-student1 = Student.Student("Jess", "Hanes", "Math", 30 ,3.0, "1234")
-student2 = Student.Student("Harry", "Miller", "Ela", 98, 4.0, "23456")
+#student1 = Student.Student("Jess", "Hanes", "Math", 30 ,3.0, "1234")
+#student2 = Student.Student("Harry", "Miller", "Ela", 98, 4.0, "23456")\
+"""
+function to write to error log file
+input:error message
+output: none
+"""
 
-students = []
+def write_to_error_log(message):
+    try:
+        #open log file
+        log_file = open("error_log.txt", "w")
+        #write error message to log file
+        log_file.write(f"{datetime.now()}: {message}\n")
+        #close log file
+        # log_file.close()err:
+        log_file.close()
+    except Exception as err:
+        print(err)
+        return
+    return
 
-students.append(student1)
-students.append(student2)
 
-for student in students:
-    student.print_data()
+def main():
+    students = []
+    try:
+        #open file
+        data_file = open("students.csv", "r")
+        #read the file line by line. create student  with each line of data
+        line = 0
+        for line_of_data in data_file:
+            if line == 0:
+                line += 1
+                continue
+            line += 1
+                #split the line by the comma
+            student_data = line_of_data.split(",")
+            #handle errors in data format. line_of_data shouldhave 6 items
+            #if error in format then write a messsage to a log file
+            try:
+                if len(student_data) != 6:
+                    raise Exception(f"There is an error in your data file on line {line}")
+            except Exception as err:
+                write_to_error_log(str(err))
+                continue
+       
+                #create a student with the split data
+            new_student = Student.Student(student_data[0], student_data[1], student_data[2], int(student_data[3]), float(student_data[4]), student_data[5].strip())
+                    #append student to student list
+            students.append(new_student)
+    except Exception as err:
+        print(err, "See log file for details")
+        
+        
+    #.strip gets rid of any extra piecesw in your printed data like \n
+
+    for the_student in students:
+        the_student.print_data()
+
+    #students.append(student1) students.append(student2)
+#if there is an error iut will dshow in error_log.txt
+    #for student in students:
+        #student.print_data()
+main()
